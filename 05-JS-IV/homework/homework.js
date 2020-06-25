c9bc2aed071f63c8d896dc50bc72d0a2bda27955
Tomas <tomasbarcojo@gmail.com>
2020-06-25 18:15:48 -0300

    Avance homework de 05-JS-IV

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -6,6 +6,14 @@ function crearGato(nombre, edad) {
   // Agrega un método (funcion) llamado "meow" que devuelva el string "Meow!"
   // Devuelve el objeto
   // Tu código:
+  var Gato = {
+    nombre: nombre,
+    edad: edad,
+    meow: function() {
+      console.log('Meow!');
+  }
+  }
+  return Gato;
 }
 
 
@@ -14,6 +22,7 @@ function agregarPropiedad(objeto, property) {
   // Devuelve el objeto
   // NOTA: El nombre de la propiedad no es "propiedad", el nombre es el valor del argumento llamado "property" (una cadena/string)
   // Tu código:
+
 }
 
 function invocarMetodo(objeto, metodo) {
